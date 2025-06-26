97c7d43d01836901666a1840f2c05e82473ce429
Eyelly Wu <eyelly.wu@gmail.com>
2025-06-26 20:19:12 +0800

    refactor(i18n): 升级 @i18n-pro/vue 至 2.0.0-alpha.1 版本
    - 更新 @i18n-pro/vue 依赖至 2.0.0-alpha.1 版本
    - 新增 i18n-pro 依赖 3.0.0-alpha.2
    - 重构 i18n 配置和使用方式
    - 更新多语言示例和实现逻辑
    - 调整项目结构和开发配置

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import {createApp} from 'vue'
+import {createI18n} from '@i18n-pro/vue'
 import App from './App.vue'
-import i18n from './i18n'
+import initI18nState from './i18n'
 
-createApp(App).use(i18n).mount('#app')
+async function init() {
+  const params = new URLSearchParams(
+    new URLSearchParams(window.location.search.slice(1)),
+  )
+  const locale = params.get('locale') || 'en'
+
+  const langs = initI18nState.langs || {}
+  if (locale !== 'zh') {
+    const lang = await (await fetch(`../i18n/${locale}.json`)).json()
+    langs[locale] = lang
+  }
+
+  const i18n = createI18n({
+    ...initI18nState,
+    langs,
+    locale,
+    with$: false
+  })
+
+ createApp(App).use(i18n).mount('#app')
+}
+
+init()
