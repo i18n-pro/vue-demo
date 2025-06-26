930c85e5950b7295d42cc22105df93af18410370
Eyelly Wu <eyelly.wu@gmail.com>
2025-06-26 20:21:40 +0800

    refactor(i18n): 重构国际化配置和加载逻辑
    - 更新 README 文件中的示例链接
    - 添加新的国际化配置项和示例文本
    - 重构 i18n 配置文件，使用 TypeScript
    - 优化 App.vue 中的国际化用法
    - 重新实现国际化状态初始化和动态加载

--- README_zh-CN.md
+++ README_zh-CN.md
@@ -1,10 +1,10 @@
 
 # 概述
-这是 `@i18n-pro/vue` 库的在线应用示例，可通过如下两种在线编辑平台访问预览
+这是 `@i18n-pro/vue@2` 库的在线应用示例，可通过如下两种在线编辑平台访问预览
 * with$ = true
-   * [Open in CodeSandbox](https://codesandbox.io/p/github/i18n-pro/vue-demo/main?file=README_zh-CN.md)
-   * [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz_small.svg "Open in StackBlitz")](https://stackblitz.com/github/i18n-pro/vue-demo/#main?file=README_zh-CN.md)
+   * [Open in CodeSandbox](https://codesandbox.io/p/github/i18n-pro/vue-demo/v2?file=README_zh-CN.md)
+   * [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz_small.svg "Open in StackBlitz")](https://stackblitz.com/github/i18n-pro/vue-demo/#v2?file=README_zh-CN.md)
 * with$ = false
-   * [Open in CodeSandbox](https://codesandbox.io/p/github/i18n-pro/vue-demo/simple?file=README_zh-CN.md)
-   * [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz_small.svg "Open in StackBlitz")](https://stackblitz.com/github/i18n-pro/vue-demo/tree/simple?file=README_zh-CN.md)
+   * [Open in CodeSandbox](https://codesandbox.io/p/github/i18n-pro/vue-demo/v2-simple?file=README_zh-CN.md)
+   * [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz_small.svg "Open in StackBlitz")](https://stackblitz.com/github/i18n-pro/vue-demo/tree/v2-simple?file=README_zh-CN.md)
 
--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,42 @@
 <template>
   <div>
-    <div v-if="show">
-      <select :value="i18nState.locale || 'zh'" @change="onSelectChange">
-        <option v-for="item in Object.entries(locales)" :value="item[0]" :key="item[0]">
-          {{ item[1] }}
-        </option>
-      </select>
-      <div class="title">{{ $t('基础示例') }}</div>
-      <div>{{ $t('简单的一段描述') }}</div>
-      <div>{{ $t('{0}是世界上最好的语言？我不信', bestProgramLang) }}</div>
-      <div>
-        {{ $t(
-          '这个男人叫{0}，意外获得了超能力，这个女人叫{1}，意外被{2}追杀，这个小孩叫{3}，意外遭遇了意外',
-          '小帅',
-          '小美',
-          'FBI',
-          '小白',
-        ) }}
-      </div>
+    <select :value="i18nState.locale || 'zh'" @change="onSelectChange">
+      <option v-for="item in Object.entries(locales)" :value="item[0]" :key="item[0]">
+        {{ item[1] }}
+      </option>
+    </select>
+    <div className="title">{{ $t('自定义 key') }}</div>
+    <div>{{ $t.t('motto', `又不是不能用`) }}</div>
 
-      <div class="title">{{ $t('格式化数字') }}</div>
-      <div>{{ $t('GitHub全球开发者数量达到了{n0}', 83000000) }}</div>
-
-      <div class="title">{{ $t('格式化金额') }}</div>
-      <div>{{ $t('售价{c0}', 123456.78) }}</div>
-
-      <div class="title">{{ $t('格式化日期') }}</div>
-      <div>{{ $t('今天的日期是{d0}', date) }}</div>
-
-      <div class="title">{{ $t('格式化时间') }}</div>
-      <div>{{ $t('当前时间：{t0}', date) }}</div>
-
-      <div class="title">{{ $t('格式化复数') }}</div>
-      <div>{{ $t('我有{p0个苹果}', 0) }}</div>
-      <div>{{ $t('我有{p0个苹果}', 1) }}</div>
-      <div>{{ $t('我有{p0个苹果}', 5) }}</div>
+    <div class="title">{{ $t('基础示例') }}</div>
+    <div>{{ $t('简单的一段描述') }}</div>
+    <div>{{ $t('{0}是世界上最好的语言？我不信', bestProgramLang) }}</div>
+    <div>
+      {{ $t(
+        '这个男人叫{0}，意外获得了超能力，这个女人叫{1}，意外被{2}追杀，这个小孩叫{3}，意外遭遇了意外',
+        '小帅',
+        '小美',
+        'FBI',
+        '小白',
+      ) }}
     </div>
+
+    <div class="title">{{ $t('格式化数字') }}</div>
+    <div>{{ $t('GitHub全球开发者数量达到了{n0}', 83000000) }}</div>
+
+    <div class="title">{{ $t('格式化金额') }}</div>
+    <div>{{ $t('售价{c0}', 123456.78) }}</div>
+
+    <div class="title">{{ $t('格式化日期') }}</div>
+    <div>{{ $t('今天的日期是{d0}', date) }}</div>
+
+    <div class="title">{{ $t('格式化时间') }}</div>
+    <div>{{ $t('当前时间：{t0}', date) }}</div>
+
+    <div class="title">{{ $t('格式化复数') }}</div>
+    <div>{{ $t('我有{p0个苹果}', 0) }}</div>
+    <div>{{ $t('我有{p0个苹果}', 1) }}</div>
+    <div>{{ $t('我有{p0个苹果}', 5) }}</div>
     <div v-if="loading" class="loading">
       loading...
     </div>
@@ -61,51 +62,15 @@ const locales = {
 }
 let timer = null
 
-async function resolveI18n(localeProp) {
-  let locale = undefined
-
-  if (localeProp) {
-    locale = localeProp
-  } else {
-    const params = new URLSearchParams(
-      new URLSearchParams(window.location.search.slice(1)),
-    )
-    locale = params.get('locale') || 'en'
-  }
-
-  let lang = {}
-  if (locale != 'zh') {
-    loading.value = true
-    let req = await fetch(`../i18n/${locale}.json`)
-    try {
-      lang = await req.json()
-    } catch (error) {
-      console.error(error)
-    }
-    loading.value = false
-  }
-  setI18n({
-    langs: {
-      [locale]: lang,
-    },
+async function onSelectChange(e) {
+  const locale = e.target.value
+  await setI18n({
     locale,
   })
-
-  show.value = true
-}
-
-function onSelectChange(e) {
-  const locale = e.target.value
-  if (i18nState.value?.langs?.[locale] || locale == 'zh') {
-    setI18n({ locale })
-  } else {
-    resolveI18n(locale)
-  }
   history.replaceState(null, '', `?locale=${locale}`)
 }
 
 onMounted(() => {
-  resolveI18n()
   timer = setInterval(() => {
     date.value = new Date()
   }, 1000)
